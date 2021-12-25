4347f71363dfe36f18ae2c07ce970a76e0834ab1
Patrick Singh <patrick.singh@bettyblocks.com>
2021-12-25 16:07:38 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{ mix('js/app.js') }}" defer></script>
+        <title>BlogQL</title>
+    </head>
+    <body class="antialiased">
+        <div id="app">
+
+        </div>
+        <h1 class="text-3xl font-bold underline decoration-sky-500">
+            Hello world!
+          </h1>
+
+    </body>
+</html>
